7cf3b2bc2d58a324f77a37d9bc2e96d820ff1b12
rifatalom97 <rifatalom97@gmail.com>
2026-09-24 10:35:11 +0600

    Src/components and src/data folder created and components added

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
-import './App.css'
+import { useState } from "react"
+import toast, {Toaster} from "react-hot-toast"
+
+
 
 function App() {
   return (
-    <>
-      
-    </>
+    <div className="min-h-screen">
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: { fontFamily: "inherit", fontSize: "14px", borderRadius: "10px" },
+          success: { iconTheme: { primary: "#c026d3", secondary: "#fff" } },
+        }}
+      />
+    </div>
   )
 }
 
